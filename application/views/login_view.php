1b7e6f898fe34801653a3a14cc09f7e4e302f68d
Tim Joosten <Topairy@gmail.com>
2014-10-23 01:13:01 +0200

    Merge branch 'master' of https://github.com/Tjoosten/Login-sys
    Conflicts:
    	README.md

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -1,19 +1,62 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="nl">
   <head>
-    <title>Simple Login with CodeIgniter</title>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    
+    <link rel="icon" href="/assets/ico/favicon.ico">
+
+    <title>Login system for CodeIgniter.</title>
   </head>
   <body>
-    <h1>Simple Login with CodeIgniter</h1>
-    <?php echo validation_errors(); ?>
-    <?php echo form_open('verifylogin'); ?>
-      <label for="username">Username:</label>
-      <input type="text" size="20" id="username" name="username"/>
-      <br/>
-      <label for="password">Password:</label>
-      <input type="password" size="20" id="passowrd" name="password"/>
-      <br/>
-      <input type="submit" value="Login"/>
-    </form>
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Login-sys</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <!-- Navigation bar -->
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="page-header">
+            <h2> Login system for CodeIgniter.</h2>
+          </div>
+
+          <?php echo validation_errors(); ?>
+
+          <?php echo form_open('verifylogin'); ?>
+            <label for="username">Username:</label>
+            <input type="text" size="20" id="username" name="username"/>
+            <br/>
+            
+            <label for="password">Password:</label>
+            <input type="password" size="20" id="passowrd" name="password"/>
+            <br/>
+
+            <input type="submit" value="Login!" />
+            <input type="reset" value="Reset!" />
+          </form>
+
+        </div>
+      </div>
+    </div>
+
+    <!-- Javascript -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
   </body>
 </html>
