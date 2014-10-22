7e25cd56ecd0686b04f57f29e9b21c66e1c1da03
Tim Joosten <Topairy@gmail.com>
2014-10-23 01:30:18 +0200

    Rebase controllers

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -1,18 +1,14 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+	class Login extends CI_Controller {
 
-class Login extends CI_Controller {
+  	function __construct() {
+    	parent::__construct();
+  	}
 
-  function __construct()
-  {
-    parent::__construct();
-  }
-
-  function index()
-  {
-    $this->load->helper('form');
-    $this->load->view('login_view');
-  }
-
-}
+  	function index() {
+    	$this->load->helper('form');
+    	$this->load->view('login_view');
+  	}
+	}
 
 ?>
